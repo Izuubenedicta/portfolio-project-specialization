5cce3c51202c9b6ea098fb87e10636d92e2bf81b
Albertbassey <aniediongetim@gmail.com>
2025-01-27 14:33:19 -0800

    component to display posts in a list format

---
+++ source/component/List.Posts.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import PartSinglePost from "./Part.SinglePost";
+
+const ListPosts = ({ posts }) => {
+  const [currentPosts, setCurrentPosts] = useState(posts);
+
+  useEffect(() => {
+    setCurrentPosts(posts);
+  }, [posts]);
+
+  const postArray = currentPosts.map((post) => (
+    <PartSinglePost key={post.id} post={post} />
+  ));
+
+  return <ul>{postArray}</ul>;
+};
+
+ListPosts.propTypes = {
+  posts: PropTypes.arrayOf(PropTypes.object),
+};
+
+ListPosts.defaultProps = {
+  posts: [],
+};
+
+export default ListPosts;
