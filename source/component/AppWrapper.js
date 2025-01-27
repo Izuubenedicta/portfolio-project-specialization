bad60d6a580be9d9437fefc8527d32e2da331ebb
Albertbassey <aniediongetim@gmail.com>
2025-01-27 13:59:37 -0800

    Implement lazy loading for MainContent component

---
+++ source/component/AppWrapper.js
@@ -0,0 +1,21 @@
+import React, { Suspense } from "react";
+import { Switch } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Header from "./Header";
+import Footer from "./Footer";
+
+const MainContent = React.lazy(() => import("./MainContent"));
+
+const AppWrapper = () => (
+  <Container>
+    <Header />
+    <Suspense fallback={<div>Loading...</div>}>
+      <Switch>
+        <MainContent />
+      </Switch>
+    </Suspense>
+    <Footer />
+  </Container>
+);
+
+export default AppWrapper;
