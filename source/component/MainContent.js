ceb50b8feb372e8d7afc75246297248c99f2da34
Albertbassey <aniediongetim@gmail.com>
2025-01-27 14:42:07 -0800

    main content pages

---
+++ source/component/MainContent.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import { Route } from "react-router-dom";
+import PageHome from "./Page.Home";
+import PagePost from "../containers/Page.Post";
+import PageComments from "../containers/Page.Comment";
+import PageNewPost from "../containers/Page.NewPost";
+import PageViewPost from "../containers/Page.ViewPost";
+
+const MainContent = () => (
+  <Container>
+    <Route exact path="/comments">
+      <PageComments />
+    </Route>
+    <Route exact path="/posts">
+      <PagePost />
+    </Route>
+    <Route exact path="/new-post">
+      <PageNewPost />
+    </Route>
+    <Route exact path="/view-post/:id" component={PageViewPost} />
+    <Route exact path="/">
+      <PageHome />
+    </Route>
+  </Container>
+);
+
+export default MainContent;
