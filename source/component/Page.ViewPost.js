736220fb0c0a01e3bfe5b27c83d1cf36cf63f19e
Albertbassey <aniediongetim@gmail.com>
2025-01-27 15:09:06 -0800

    ViewPost page to display a single post with delete functionality

---
+++ source/component/Page.ViewPost.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import PropTypes from "prop-types";
+import SinglePost from "./Part.SinglePost";
+
+const PageViewPost = ({ post, deletePost }) => (
+  <Container fluid className="p-0">
+    <h1>View Post</h1>
+    <SinglePost key={post.id} post={post} deletePost={deletePost} cardItem />
+  </Container>
+);
+
+PageViewPost.propTypes = {
+  deletePost: PropTypes.func.isRequired,
+  post: PropTypes.shape({
+    id: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  }).isRequired,
+};
+
+PageViewPost.defaultProps = {};
+
+export default PageViewPost;
