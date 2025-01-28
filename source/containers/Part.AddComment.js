18bf0873dc8bf91b84ceed8fa60f1f0a4b17538f
Izuubenedicta <163085809+Izuubenedicta@users.noreply.github.com>
2025-01-28 05:14:14 +0100

    Add AddCommentContainer component to handle comment submission for posts and redirection

---
+++ source/containers/Part.AddComment.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import PropTypes from "prop-types";
+import axios from "../utils/axios-default";
+import AddComment from "../components/Part.AddComment";
+
+class AddCommentContainer extends Component {
+  /**
+   * Submit a comment for the current post.
+   */
+  submitComment = async (values) => {
+    const { history } = this.props;
+    const { postId } = this.props;
+    await axios.post("/comments", { comment: { ...values, postId } });
+    history.push(`/view-post/${postId}/refresh`);
+    history.goBack();
+  };
+
+  render() {
+    return <AddComment onSubmit={this.submitComment} />;
+  }
+}
+
+AddCommentContainer.propTypes = {
+  history: PropTypes.shape({
+    goBack: PropTypes.func.isRequired,
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+  postId: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+};
+
+AddCommentContainer.defaultProps = {
+  postId: 0,
+};
+
+export default withRouter(AddCommentContainer);
