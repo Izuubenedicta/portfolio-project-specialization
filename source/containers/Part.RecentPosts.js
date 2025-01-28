841a2fa02dbbdde9b97fd40d929627d016a78e2d
Izuubenedicta <163085809+Izuubenedicta@users.noreply.github.com>
2025-01-28 05:16:17 +0100

    Add RecentPostsContainer component to fetch and display the 5 most recent posts on the landing page

---
+++ source/containers/Part.RecentPosts.js
@@ -0,0 +1,38 @@
+import React, { Component, Suspense } from "react";
+import axios from "../utils/axios-default";
+import ListPosts from "../components/List.Posts";
+
+class RecentPostsContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+    };
+  }
+
+  async componentDidMount() {
+    await this.getPosts();
+  }
+
+  /**
+   * Request for the 5 most recent posts in descending order.
+   */
+  getPosts = async () => {
+    const { data } = await axios.get("/posts?limit=5&order=DESC");
+    this.setState({ posts: data });
+  };
+
+  render() {
+    const { posts } = this.state;
+    return (
+      <>
+        <Suspense fallback={<div>Loading...</div>}>
+          <ListPosts posts={posts} />
+        </Suspense>
+      </>
+    );
+  }
+}
+
+export default RecentPostsContainer;
+~                                      
