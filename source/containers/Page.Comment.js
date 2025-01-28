d73316e54c292c25489aad2e5e59d4d203508acd
Izuubenedicta <163085809+Izuubenedicta@users.noreply.github.com>
2025-01-28 05:01:11 +0100

    Add PageCommentContainer component to fetch and display comments with suspense fallback

---
+++ source/containers/Page.Comment.js
@@ -0,0 +1,37 @@
+import React, { Component, Suspense } from "react";
+import axios from "../utils/axios-default";
+import PageComment from "../components/Page.Comment";
+
+class PageCommentContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      comments: [],
+    };
+  }
+
+  async componentDidMount() {
+    await this.getComments();
+  }
+
+  /**
+   * Request all comments.
+   */
+  getComments = async () => {
+    const { data } = await axios.get("/comments");
+    this.setState({ comments: data });
+  };
+
+  render() {
+    const { comments } = this.state;
+    return (
+      <>
+        <Suspense fallback={<div>Loading...</div>}>
+          <PageComment comments={comments} />
+        </Suspense>
+      </>
+    );
+  }
+}
+
+export default PageCommentContainer;
